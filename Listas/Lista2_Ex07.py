ab443eea29de819ff5f220033c6c06f61f32ff39
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-09-28 00:06:12 -0300

    Create Lista2_Ex07.py

---
+++ Listas/Lista2_Ex07.py
@@ -0,0 +1,22 @@
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+print()
+print("*"*50)
+print()
+
+if n1>n2 and n1>n3:
+	print(f'O maior numero digitado foi {n1}')
+if n1<n2 and n1<n3:
+	print(f'O menor numero digitado foi {n1}')	
+
+if n2>n1 and n2>n3:
+	print(f'O maior numero digitado foi {n2}')
+if n2<n1 and n2<n3:
+	print(f'O menor numero digitado foi {n2}')	
+
+if n3>n1 and n3>n2:
+	print(f'O maior numero digitado foi {n3}')
+if n3<n1 and n3<n2:
+	print(f'O menor numero digitado foi {n3}')	
