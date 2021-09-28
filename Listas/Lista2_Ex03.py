e161637f42879b9ab1218fd84825201de0acb8f4
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-09-27 23:38:08 -0300

    Create Lista2_Ex03.py

---
+++ Listas/Lista2_Ex03.py
@@ -0,0 +1,11 @@
+
+a=input('Digite F para femino e M para masculino: ')
+
+if a=="F":
+	print("F - Feminino")
+
+if a == "M":
+	print("M - Masculino")
+
+if a != "F" and a != "M":
+	print("Sexo inválido")
