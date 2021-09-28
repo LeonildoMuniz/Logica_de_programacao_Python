b04b7aca1fa1d275d6f6d54ad172d6ad650c9e55
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-09-27 23:46:05 -0300

    Create Lista2_Ex04.py

---
+++ Listas/Lista2_Ex04.py
@@ -0,0 +1,9 @@
+
+a=input('Digite uma letra: ')
+
+if a=="a" or a=="e" or a=="i" or a=="o" or a=="u"\
+or a=="A" or a=="E" or a=="I" or a=="O" or a=="U":
+
+	print("Letra digitada é uma vogal!")
+else:
+	print("Letra digitada é uma consoante!")
