6a321c5cf93a7893f6ab6fd4e4eb38a1cc82cd84
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-09-28 00:35:36 -0300

    Create Lista2_Ex10.py

---
+++ Listas/Lista2_Ex10.py
@@ -0,0 +1,13 @@
+a=input('Digite M-matutino ou V-Vespertino ou N- Noturno: ')
+
+if a=="M":
+	print("Bom dia!")
+
+if a=="V":
+	print("Boa tarde!")		
+
+if a=="N":
+	print("Boa noite!")
+
+if a!="M" and a!="V" and a!="N":
+	print("Valor Inválido")
