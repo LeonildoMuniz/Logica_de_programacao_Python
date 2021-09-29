4672b53ada1eb569b6c1ac43b35edc3bea26d325
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-09-29 13:52:39 -0300

    Create Lista3_Ex35.py

---
+++ Listas/Lista3_Ex35.py
@@ -0,0 +1,21 @@
+cont=0
+vet=[]
+vet2=[]
+num1=int(input('Verificar numeros primos do intervano 1 a : '))
+
+for i in range(num1):
+	vet.append(i+1)
+
+
+for i in range(0,num1):
+	for j in range(0,num1):
+		if vet[i]%(j+1)==0:
+			cont+=1
+	
+	if cont == 2:
+		vet2.append(vet[i])
+	cont=0
+
+print()
+print('='*50)
+print(f'Numeros primos encontrados:\n {vet2}')
