7f318ad29810ba6b4e49489c67bddbcf530b3083
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2021-09-29 23:35:30 -0300

    Update Lista3_Ex50.py

--- Listas/Lista3_Ex50.py
+++ Listas/Lista3_Ex50.py
@@ -1,11 +1,11 @@
 a=1
-b=1
+b=2
 result=0
 num=int(input('Informe o numeros de termos para serie: '))
 
 for i in range(0,num,1):
 	result+=a+(a/b)
 	print(f'{i+1} - H = {a}+{a}/{b}')	
-	b+=2
+	b+=1
 
 print(f'Soma total de serie: {result:.2f}')
